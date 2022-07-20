de96691f9351dcb00d8e246e0fda9807c75a0136
baldgenie <siddhartha.kapoor@gmail.com>
2022-07-21 00:18:59 +0545

    commit

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
 
     try {
         await res.revalidate('/')
+        await res.revalidate('/photo')
         console.log('Re-Validating... Successfull');
         return res.json({ revalidated: true })
     } catch (error) {
